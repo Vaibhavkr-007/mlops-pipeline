9b3d7c5bc9db4d632cc3df14cb9ba2abfc681b8e
Vaibhav Kumar Mahto <118665260+Vaibhavkr-007@users.noreply.github.com>
2025-04-10 09:45:41 +0530

    Update app.py

--- web_app/app.py
+++ web_app/app.py
@@ -52,7 +52,7 @@ request_time = Summary('request_processing_seconds', 'Time spent processing requ
 def metrics():
     return request_time.collect()
 
-mlflow.set_tracking_uri("http://16.171.54.43:5000")
+mlflow.set_tracking_uri("http://13.48.149.188:5000")
 # mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment("Loan_Prediction_")
 
